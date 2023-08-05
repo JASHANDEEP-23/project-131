be63907b9b6fa0f53e9ee2f2484724ba791d6d1c
JASHANDEEP-23 <84918095+JASHANDEEP-23@users.noreply.github.com>
2023-08-05 17:45:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+//function preload(){
+//img = loadImage('dog_cat.jpg')
+//}
+
+//function setup(){
+//canvas = createCanvas(400,300)
+//canvas.center()
+
+
+
+//}
+
+//function draw() {
+  //  //image( name, x , y, width, height)
+//image(img , 0 , 0 , 400 , 300) 
+//noFill()
+//stroke('#25147a')
+//strokeWeight(3)
+//rect( 13 , 30 , 200, 265)
+//fill('aquamarine')
+//text('DOG', 15 , 25)
+//}
